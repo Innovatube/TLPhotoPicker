5ea9e00fd99a4ed28cf255c09185f8e99302762c
Brian <brian.tran@innovatube.com>
2018-05-25 17:59:43 +0700

    fix bugs.

--- TLPhotoPicker/Classes/CameraViewController.swift
+++ TLPhotoPicker/Classes/CameraViewController.swift
@@ -25,6 +25,14 @@ class CameraViewController: UIViewController {
     var stillImageOutput: AVCaptureStillImageOutput?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
 
+    public init() {
+        super.init(nibName: "CameraViewController", bundle: Bundle(for: CameraViewController.self))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
